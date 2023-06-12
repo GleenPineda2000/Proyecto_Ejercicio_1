76ebd9004075a1f6f10605916d811ba9e0f4b109
GleenPineda2000 <gleenpineda2000@gmail.com>
2023-06-11 20:29:25 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Ejercicio_1/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Proyecto_Ejercicio_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+private async void btn_guardar(object sender, EventArgs e)
+        {
+
+            String nombre = nombres.Text;
+            String apellido = apellidos.Text;
+            String edades = edad.Text;
+            String correo = correos.Text;
+
+            await Navigation.PushAsync(new Pagina_2(nombre, apellido, edades, correo));
+
+
+
+        }
+    }
+}
---
+++ Proyecto_Ejercicio_1/Pagina_2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proyecto_Ejercicio_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina_2 : ContentPage
+    {
+        public Pagina_2(String nombre, String apellido,String edades, String correo )
+        {
+            InitializeComponent();
+            datos.Text = "Datos" + nombre + " " + apellido + " " + edades + " " + correo;
+
+        }
+    }
+}
